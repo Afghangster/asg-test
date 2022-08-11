9789c3460531d5befea7669f3a41047034feb23e
Mielad <mniekzad@gmail.com>
2022-08-11 22:02:00 +0400

    remove s3 bucket acl

--- s3.tf
+++ s3.tf
@@ -1,13 +1,8 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "bucket-x" {
+  bucket = "tf-test-bucket"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
 }
