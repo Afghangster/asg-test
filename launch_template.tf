5014dce095a5bc7e3e60748e1fa863633d388b43
Mielad <mniekzad@gmail.com>
2022-08-11 16:53:00 +0400

    insert code into file

--- launch_template.tf
+++ launch_template.tf
@@ -1,8 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+resource "aws_launch_template" "test-configuration" {
+  name_prefix   = "test-configuration"
+  image_id      = "ami-0b0ea68c435eb488d"
+  instance_type = "t3.micro"
 }
